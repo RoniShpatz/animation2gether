b7315e9837a1cd5457714c685f94c32de7628af0
ShpatzR <roni1kohen@gmail.com>
2025-02-05 23:42:44 +0000

    all

--- animation2gether/settings.py
+++ animation2gether/settings.py
@@ -102,16 +102,7 @@ DATABASES = {
         conn_health_checks=True,
     )
 }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.environ.get('DB_NAME'),
-#         'USER': os.environ.get('DB_USER'),
-#         'PASSWORD': os.environ.get('DB_PASSWORD'),
-#         'HOST': os.environ.get('DB_HOST', 'localhost'),
-#         'PORT': os.environ.get('DB_PORT', '5432'),
-#     }
-# }
+
 
 
 # Password validation
--- animation2gether/urls.py
+++ animation2gether/urls.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
-# from . import views
+
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
